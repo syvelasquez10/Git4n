a757efac2fce4230ae0819c1ab6df70af415a179
Sergio Velasquez <sy.velasquez10@uniandes.edu.co>
2020-12-21 15:38:44 -0500

    closes #1

---
+++ src/footer/Footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Footer.css'
+
+export function Footer() {
+	return (
+		<div className="footer">
+			Develop by <a href="https://www.linkedin.com/in/sergio-yodeb-velasquez-yepes-49583a103/"> Sergio Velasquez</a>
+		</div>
+	);
+}
